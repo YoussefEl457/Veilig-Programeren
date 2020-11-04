c65224c1d536325deebae3621aa8278f946fb3b1
Youssef <youssefelouamari73@gmail.com>
2020-11-04 15:23:49 +0100

    Veilig Programeren opdracht 1

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+
+include "db.php";
+
+$username = $_POST["Username"];
+$password = $_POST["Password"];
+
+print $username, $password;
+
+
+
+$myConn = new DB;
+
+
+$query = "SELECT * FROM user WHERE username = $_post["Username"]";
+
+$result = $myConn->executeSQL($query);
+
+gettype($result);
+
+if (!empty($result)) {
+    echo "<br> Login as $username <br>";
+    //Het laat info zien dat is opgeslagen in de variable.
+    print_r($result);
+} else {
+    echo "<br> Invalid login! <br>";
+}
+
+?>
